b0e051691043c31863610351b84634b262f141e2
samiha <boudherouasamiha@gmail.com>
2026-09-17 13:14:21 +0100

    Fix: ajout de session.user.id via callbacks NextAuth + posts CRUD

--- auth.ts
+++ auth.ts
@@ -33,6 +33,23 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             },
         }),
     ],
+
+
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token.id = user.id
+            }
+            return token
+        },
+        async session({ session, token }) {
+            if (session.user) {
+                session.user.id = token.id as string
+            }
+            return session
+        },
+    },
+
     pages: {
         signIn: "/login",
     },
